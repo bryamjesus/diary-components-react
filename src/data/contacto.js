60eb63dd58fac1a340848cf1f1e44a51583699c0
Bryam Talledo <bryamtalledog@gmail.com>
2022-10-05 20:12:20 -0500

    Add contacto.js

---
+++ src/data/contacto.js
@@ -0,0 +1,5 @@
+export default data = [
+  { id: 1, nombres: 'Juan', apellidos: 'Paredes', telefono: '987654123' },
+  { id: 2, nombres: 'Carlos', apellidos: 'Zapata', telefono: '912321874' },
+  { id: 3, nombres: 'Karina', apellidos: 'Gonzales', telefono: '928763432' },
+]
