9420af30b2d4a9ff4c76219961711cb6991dc742
Bryam Talledo <bryamtalledog@gmail.com>
2022-10-05 21:27:38 -0500

    Update

--- src/data/contacto.js
+++ src/data/contacto.js
@@ -1,7 +1,7 @@
 export const data = [
-  { id: 1, nombres: 'Juan', apellidos: 'Paredes', telefono: '987654123', direccion: 'San Isidro' },
-  { id: 2, nombres: 'Carlos', apellidos: 'Zapata', telefono: '912321874', direccion: 'San Isidro' },
-  { id: 3, nombres: 'Karina', apellidos: 'Gonzales', telefono: '928763432', direccion: 'San Isidro' },
+  { id: 1, nombres: 'Juan', apellidos: 'Paredes', telefono: '987654123', direccion: 'San Isidro', email:'jparedes@mail.com' },
+  { id: 2, nombres: 'Carlos', apellidos: 'Zapata', telefono: '912321874', direccion: 'Los Olivos', email:'czapata@mail.com' },
+  { id: 3, nombres: 'Karina', apellidos: 'Gonzales', telefono: '928763432', direccion: 'Huacho', email:'kgonzales@mail.com' },
 ] /* import { data } from './data/contacto' */
 
 // const data = [
