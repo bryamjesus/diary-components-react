80edeb59690bf425fed40162210f0d537afe7b14
Bryam Talledo <bryamtalledog@gmail.com>
2022-10-05 20:41:05 -0500

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { data } from './data/contacto'
 
 const App = () => {
   return (
-    <div className="">
-      {data.map((x) => x.id)}
+    <div>
+      <nav class="navbar bg-dark">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Agendad de contactos</span>
+        </div>
+      </nav>
+      <div className="">
+
+        {data.map((x) => x.id)}
+      </div>
     </div>
-    
   )
 }
 
