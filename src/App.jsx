9b756978e59a6ac4e19ec1576a33711fd55bf33f
Bryam Talledo <bryamtalledog@gmail.com>
2022-10-05 21:02:46 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,57 @@ import { data } from './data/contacto'
 const App = () => {
   return (
     <div>
-      <nav class="navbar bg-dark">
-        <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1">Agendad de contactos</span>
+      <nav className="navbar bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Agendad de contactos</span>
         </div>
       </nav>
-      <div className="">
 
-        {data.map((x) => x.id)}
+      <div className="container">
+        <div className='row mt-3'>
+          <div className='col-md-8'>
+            <h3>Lista de contáctos</h3>
+            {data.map((x) => (
+              <div key={x.id} className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src="https://rickandmortyapi.com/api/character/avatar/250.jpeg" className="img-fluid rounded-start" alt="..." />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">{x.nombres} {x.apellidos}</h5>
+                      <p className="card-text">
+                      <i className="bi bi-telephone-fill"></i> {x.telefono}
+                      </p>
+                      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='col-md-4'>
+            <h3>Nuevo contácto</h3>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 export default App
+/*
+<div className='row mt-3'>
+      <div className='col-md-8'>
+        <h3>Lista de contáctos</h3>
+        {data.map((item)=>(
+          <div>
+              <div>{item.nombres}</div>
+              <div>{item.apellidos}</div>
+          </div>
+        ))}
+      </div>
+      <div className='col-md-4'>
+        <h3>Nuevo contácto</h3>
+      </div>
+    </div>*/
--- src/data/contacto.js
+++ src/data/contacto.js
@@ -1,7 +1,7 @@
 export const data = [
-  { id: 1, nombres: 'Juan', apellidos: 'Paredes', telefono: '987654123' },
-  { id: 2, nombres: 'Carlos', apellidos: 'Zapata', telefono: '912321874' },
-  { id: 3, nombres: 'Karina', apellidos: 'Gonzales', telefono: '928763432' },
+  { id: 1, nombres: 'Juan', apellidos: 'Paredes', telefono: '987654123', direccion: 'San Isidro' },
+  { id: 2, nombres: 'Carlos', apellidos: 'Zapata', telefono: '912321874', direccion: 'San Isidro' },
+  { id: 3, nombres: 'Karina', apellidos: 'Gonzales', telefono: '928763432', direccion: 'San Isidro' },
 ] /* import { data } from './data/contacto' */
 
 // const data = [
