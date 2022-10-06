74e476cb32f0176857298f8f2a52bbce52564284
Bryam Talledo <bryamtalledog@gmail.com>
2022-10-05 20:33:57 -0500

    Import data

--- src/data/contacto.js
+++ src/data/contacto.js
@@ -1,5 +1,13 @@
-export default data = [
+export const data = [
   { id: 1, nombres: 'Juan', apellidos: 'Paredes', telefono: '987654123' },
   { id: 2, nombres: 'Carlos', apellidos: 'Zapata', telefono: '912321874' },
   { id: 3, nombres: 'Karina', apellidos: 'Gonzales', telefono: '928763432' },
-]
+] /* import { data } from './data/contacto' */
+
+// const data = [
+//   { id: 1, nombres: 'Juan', apellidos: 'Paredes', telefono: '987654123' },
+//   { id: 2, nombres: 'Carlos', apellidos: 'Zapata', telefono: '912321874' },
+//   { id: 3, nombres: 'Karina', apellidos: 'Gonzales', telefono: '928763432' },
+// ]
+
+// export default data /* import data from './data/contacto' */
